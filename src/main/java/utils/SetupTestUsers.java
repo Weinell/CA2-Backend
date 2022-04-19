75b80872bf41ad3db12f8b9d07376d53d1160372
TheLazzoro <43842641+TheLazzoro@users.noreply.github.com>
2022-04-19 10:50:44 +0200

    Bcrypt

--- src/main/java/utils/SetupTestUsers.java
+++ src/main/java/utils/SetupTestUsers.java
@@ -20,9 +20,9 @@ public class SetupTestUsers {
     // Also, either delete this file, when users are created or rename and add to .gitignore
     // Whatever you do DO NOT COMMIT and PUSH with the real passwords
 
-    User user = new User("user", "test");
-    User admin = new User("admin", "test");
-    User both = new User("user_admin", "test");
+    User user = new User("user", "MyPassword");
+    User admin = new User("admin", "MyPassword");
+    User both = new User("user_admin", "MyPassword");
 
     if(admin.getUserPass().equals("test")||user.getUserPass().equals("test")||both.getUserPass().equals("test"))
       throw new UnsupportedOperationException("You have not changed the passwords");
@@ -41,8 +41,8 @@ public class SetupTestUsers {
     em.persist(both);
     em.getTransaction().commit();
     System.out.println("PW: " + user.getUserPass());
-    System.out.println("Testing user with OK password: " + user.verifyPassword("test"));
-    System.out.println("Testing user with wrong password: " + user.verifyPassword("test1"));
+    System.out.println("Testing user with OK password: " + user.verifyPassword("MyPassword"));
+    System.out.println("Testing user with wrong password: " + user.verifyPassword("MyPassword"));
     System.out.println("Created TEST Users");
    
   }
